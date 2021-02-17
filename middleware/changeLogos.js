945c6e04116a6c51833b10aedb7117e24fac45eb
Reynold Osei Adade <Reynold@walulel.com>
2021-02-17 17:04:04 +0000

    updated config

--- middleware/changeLogos.js
+++ middleware/changeLogos.js
@@ -4,14 +4,14 @@ export default ({ route, store }) => {
 	if (redirect_url) {
 		if (redirect_url.includes('communicate')) {
 			return store.dispatch('components/setUiConfig', {
-				color: 'wa-orange',
+				color: 'waOrange',
 				logo: 'WaCommunicate.svg',
 				caption:
 					'WaCommunicate is a social network designed for the neighbourhood community',
 			});
 		} else if (redirect_url.includes('insight')) {
 			return store.dispatch('components/setUiConfig', {
-				color: 'wa-blue',
+				color: 'waBlue',
 				logo: 'WaInsight.svg',
 				caption:
 					'WaInsight enables residential Real Estate market participants to understand the quality of every neighbourhood, postcode or zip code.',
