53bffdc7386fb83e63b279153f013ecc6c09addf
Gyen Abubakar Sadick <gyen@walulel.com>
2021-02-25 16:17:04 +0000

    Check preferred products before redirecting

--- middleware/checkLogin.js
+++ middleware/checkLogin.js
@@ -1,11 +1,15 @@
 import getCorrectDomain from '../assets/js/getCorrectDomain';
+import getDestinationAfterRegister from '~/assets/js/getDestinationAfterRegister';
 
 export default ({ req, app, env, route, redirect }) => {
 	const { redirect_url } = route.query;
 	if (!redirect_url) {
 		const token = app.$cookies.get('s_token');
 		if (token) {
-			const redirectDestination = getRedirectDestination(req);
+			const redirectDestination = getRedirectDestination(
+				req,
+				app.$cookies.get('interested_products')
+			);
 
 			if (route.path !== '/profile')
 				return window.location.replace(
@@ -20,7 +24,7 @@ export default ({ req, app, env, route, redirect }) => {
 	}
 };
 
-function getRedirectDestination(req) {
+function getRedirectDestination(req, products) {
 	let domain = '';
 	let redirectDestination = '';
 
@@ -30,11 +34,9 @@ function getRedirectDestination(req) {
 		domain = getCorrectDomain(false, window);
 	}
 
-	if (domain === 'https://staging.walulel.com') {
-		redirectDestination = `https://staging.wa-communicate.com/`;
-	} else {
-		redirectDestination = `https://wa-communicate.com/`;
-	}
+	const isStaging = domain === 'https://staging.walulel.com';
+
+	redirectDestination = getDestinationAfterRegister(isStaging, products);
 
 	return redirectDestination;
 }
